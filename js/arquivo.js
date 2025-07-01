8ea97a31762432de3c3d79a1bb4695b4964fc2ee
meloclarissag <20241ctb0100079@estudantes.ifpr.edu.br>
2025-07-01 12:26:52 -0300

    Aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,25 @@
+var global = "variavel global";
+console.log(global);
+
+function exemplo2(){
+    var local = "varivael local";
+    console.log(local);
+}
+
+exemplo2();
+
+let x = 10;
+
+if (x>5) {
+    let y = 20;
+    console.log("valor de y: ", y);
+}
+
+console.log(x);
+console.log(y);
+
+
+
+function saudacao(){
+    alert("Bem vindo ao nosso site");
+}
